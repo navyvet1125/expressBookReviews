ff1ffb6fdd17288a0704b15f696ffa429edef3da
Evan Washington <enavy04@gmail.com>
2024-04-27 23:04:44 -0700

    removed stringify from responses

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,33 +33,33 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/', async (req, res) => res.status(200).send(books));
+public_users.get('/', async (req, res) => res.status(200).json(books));
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   let isbn = req.params.isbn;
   let book = books.findBy("isbn", isbn)[0];
-  return res.status(200).send(JSON.stringify(book, null, 2));
+  return res.status(200).json(book);
 });
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   let author = req.params.author;
   let book = books.findBy("author", author);
-  return res.status(200).send(JSON.stringify(book, null, 2));
+  return res.status(200).json(book);
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   let title = req.params.title;
   let book = books.findBy("title", title)[0];
-  return res.status(200).send(JSON.stringify(book, null, 2))
+  return res.status(200).json(book)
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   let book = books.findBy("isbn", req.params.isbn)[0];
-  return res.status(200).send(JSON.stringify(book.reviews, null, 2));
+  return res.status(200).json(book.reviews);
 });
 
 module.exports.general = public_users;
